0ef76ae32404324fbbd1e79ebf84b771d058e119
Georgi Chulkov <georgi.chulkov@sap.com>
2025-07-17 17:30:15 +0200

    Update utils

--- internal/util/utils.go
+++ internal/util/utils.go
@@ -1,12 +1,11 @@
 package util
 
 import (
-	"context"
+	"encoding/hex"
+	"hash"
 	"os"
 	"slices"
 	"strings"
-
-	"github.com/gardenlinux/glci/internal/log"
 )
 
 // Ptr returns a pointer to any avlue, including a literal.
@@ -24,15 +23,20 @@ func Subset[T comparable](original, subset []T) []T {
 }
 
 // CleanEnv unsets all environment variables that start with a given prefix.
-func CleanEnv(ctx context.Context, prefix string) {
+func CleanEnv(prefix string) {
 	for _, envVar := range os.Environ() {
 		k, _, ok := strings.Cut(envVar, "=")
 		if !ok {
 			continue
 		}
 		if strings.HasPrefix(k, prefix) {
-			log.Debug(ctx, "Unsetting environment variable", "var", k)
 			_ = os.Unsetenv(k)
 		}
 	}
+}
+
+// Hash returns a string hash of a string calculated using the provided hash function.
+func Hash(h hash.Hash, str string) string {
+	_, _ = h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(make([]byte, 0, h.Size())))
 }
